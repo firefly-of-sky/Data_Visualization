706c80b04bbea17825ffcde77836e655f177927a
firefly_of_sky <2391969043@qq.com>
2026-05-10 23:09:24 +0800

    实现随机散点图

--- rw_visual.py
+++ rw_visual.py
@@ -11,9 +11,17 @@ while(True):
     # 将所有点都绘制出来
 
     plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=144)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors='none', s=1)
     ax.set_aspect('equal')
+
+    # 突出起点和终点
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], edgecolors='none', s=100)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
