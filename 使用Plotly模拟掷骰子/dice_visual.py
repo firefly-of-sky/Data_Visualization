7ddbabc0bd3b74212173dbae5a5fa2818798aad3
firefly <firefly_of_sky33550336@outlook.com>
2026-05-16 12:33:29 +0800

    将for循环改为列表推导式

--- 使用Plotly模拟掷骰子/dice_visual.py
+++ 使用Plotly模拟掷骰子/dice_visual.py
@@ -8,17 +8,13 @@ die_2 = Die(8)
 
 # 掷几次骰子并将结果存储在一个列表中
 results = []
-for roll_num in range(10_000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for roll_num in range(10_000)]
 
 # 分析结果
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
 poss_results = range(2, max_result+1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in poss_results]
 
 # 对结果进行可视化
 title = "Results of Rolling a D6 and a D 10 50,000 Times"
