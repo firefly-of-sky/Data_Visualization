59dc40e0e36844b909be9658b555ee2e7fa1160b
firefly <firefly_of_sky33550336@outlook.com>
2026-05-17 17:04:17 +0800

    图形化hn

--- 使用API/hn_submission.py
+++ 使用API/hn_submission.py
@@ -1,4 +1,5 @@
 from operator import itemgetter
+import plotly.express as px
 
 import requests
 
@@ -10,7 +11,7 @@ print(f"Status code: {r.status_code}")
 # 处理有关每篇文章的信息
 submission_ids = r.json()
 submission_dicts = []
-for submission_id in submission_ids[:5]:
+for submission_id in submission_ids[:20]:
     # 对于每篇文章，都执行一个API调用
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
@@ -20,7 +21,7 @@ for submission_id in submission_ids[:5]:
     # 对于每篇文章，都创建一个字典
     submission_dict = {
         'title': response_dict['title'],
-        'hn_link': f"https//news.ycombinator.com/item?id={submission_id}",
+        'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
         'comments': response_dict['descendants'],
     }
     submission_dicts.append(submission_dict)
@@ -28,7 +29,29 @@ for submission_id in submission_ids[:5]:
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                           reverse=True)
 
+article_links, comment_counts, hover_texts = [], [], []
 for submission_dict in submission_dicts:
-    print(f"\nTitle: {submission_dict['title']}")
-    print(f"Disscussion link: {submission_dict['hn_link']}")
-    print(f"Comments: {submission_dict['comments']}")
+    title = submission_dict['title'][:30]
+    discussion_link = submission_dict['hn_link']
+    article_link = f'<a href="{discussion_link}"">{title}</a>'
+    comment_count = submission_dict['comments']
+
+    article_links.append(article_link)
+    comment_counts.append(comment_count)
+
+    # 制作悬停信息
+    hover_texts.append(submission_dict['title'])
+
+
+# 可视化
+title = "Most commentted article in hn"
+labels = {'x':'Article', 'y': 'Comment_count'}
+fig = px.bar(x=article_links, y=comment_counts, hover_name=hover_texts, labels=labels,
+             title=title)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
+        yaxis_title_font_size=20)
+
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
+
+fig.show()
